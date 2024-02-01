4dd808b102f31fc4d8333621ed2320a4170fee49
Nicolas Fandiño <nicolasfandi@hotmail.com>
2024-01-31 23:40:40 -0300

    2da Preentrega

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
-/* Algoritmo con condicional*/
-class Alimento{
-        constructor (nombre, precio, cantidad){
-            this.nombre = nombre;
-            this.precio = precio;
-            this.cantidad = cantidad;
+let productos = [
+    { id: 1, nombre: "RAZA", precio: 2400 },
+    { id: 2, nombre: "ROYAL", precio: 3400 },
+    { id: 3, nombre: "EUKANUBA", precio: 6400 },
+    { id: 4, nombre: "EXCELENT", precio: 4300 },
+    { id: 5, nombre: "PREMIUM", precio: 8000 },
+    { id: 6, nombre: "CAT CHOW", precio: 3700 },
+    { id: 7, nombre: "PRO PLAN", precio: 5900 },
+];
+document.addEventListener("DOMContentLoaded", function(){
+const encontrar = (nombre) => {
+    let productoEncontrado;
+    for (const producto of productos) {
+        if (nombre.toUpperCase().includes(producto.nombre)) {
+            productoEncontrado = producto;
+            break;
         }
-        vender(){
-            if(this.cantidad !=0){
-            this.cantidad -=1;
-        }
-        else{
-            console.log("No hay sufieciente stock para vender")
-        }
+    };
+    if (productoEncontrado) {
+        alert(`Nombre: ${productoEncontrado.nombre}\nPrecio: $${productoEncontrado.precio}`);
+    } else {
+        alert("No se ha encontrado el producto, intente de nuevo");
     }
+};
+
+let nombre = prompt("Ingrese el producto el cual quiere conocer el precio");
+
+if (nombre) {
+    encontrar(nombre.toUpperCase());
+} else {
+    alert("No se ha encontrado el producto, intente de nuevo");
+};
+const filtrar = (precio) => {
+    let productosFiltrados = productos.filter(producto => producto.precio < precio);
+    if (productosFiltrados.length > 0) {
+        alert(`Productos con precio menor a $${precio}` +
+            productosFiltrados.map(producto => `\n Nombre: ${producto.nombre}\nPrecio: $${producto.precio}`).join('\n'));
+    } else {
+        alert(`No se ha encontrado productos con precio menor a $${precio}`);
+    }
+};
+
+let precio = prompt("Ingrese el precio maximo");
+if (precio) {
+    filtrar(parseFloat(precio));
+} else {
+    alert("No se ha ingresado un precio real, Por favor intente de nuevo");
 }
-const DogChow = new Alimento("Dog Chow",29000,1);
-console.log(DogChow);
-DogChow.vender();
-console.log(DogChow);
-DogChow.vender();
-console.log(DogChow);
-
-
-/*Algoritmo con ciclo*/
-for (let i=2;i<=6;i=i+2){
-    console.log("Numeros pares: " + i);
-}
-
-
-/*Simulador interactivo de edad*/ 
-let nac =prompt("Ingrese su año de nacimiento");
-let edad= 2024-nac;
-alert("Usted tiene "+edad + " Años");
+})
